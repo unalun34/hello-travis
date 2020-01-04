4d5214f3666a966409c2158fe5cabf472dcdf1c9
UnalUN <unal.un@hotmail.com>
2020-01-04 14:49:39 +0300

    common update

--- hello-travis/MathService/Common.cs
+++ hello-travis/MathService/Common.cs
@@ -6,8 +6,9 @@ namespace MathService
     {
         public bool IsNegative(int number)
         {
-            return false;
+            return number < 0;
         }
+
         public bool IsEven(int number)
         {
             return number % 2 == 0;
