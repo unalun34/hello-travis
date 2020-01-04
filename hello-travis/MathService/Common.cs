d2ac06b39c582222968e1fc006d9138db480e4f9
UnalUN <unal.un@hotmail.com>
2020-01-04 14:39:35 +0300

    travis projesi

---
+++ hello-travis/MathService/Common.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MathService
+{
+    public class Common
+    {
+        public bool IsNegative(int number)
+        {
+            return false;
+        }
+        public bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
